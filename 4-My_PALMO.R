77c983dda0cc8db4970945c8f78dfd730e6ec4e6
JhGoodLuck <44935987+JhGoodLuck@users.noreply.github.com>
2025-06-14 15:29:53 +0800

    Update and rename 3-My_PALMO.R to 4-My_PALMO.R

--- 4-My_PALMO.R
+++ 4-My_PALMO.R
@@ -1,3 +1,6 @@
+### This script uses directory paths defined in configure.env.
+### Please ensure the configuration file is correctly set up before running.
+
 library("PALMO")
 library("Hmisc")
 library("ggpubr")
@@ -7,11 +10,15 @@ library("circlize")
 library("ComplexHeatmap")
 library("ggrepel")
 library("pbapply")
+library("Seurat")
 
-CVD_list <- fread('CVD_gene.list',data.table=F)
+readRenviron("configure.env")
+Stable_gene_FOLDER <- Sys.getenv("Stable_gene_FOLDER")
+CVD_list <- fread(file.path(diagnostic_model_FOLDER,'CVD_gene.list'),data.table=F)
+
 #####bulk protein#####
-load("AIFI-Olink-NPX_log2_Protein.Rda")
-load("AIFI-Metadata.Rda")
+load(file.path(diagnostic_model_FOLDER, "AIFI-Olink-NPX_log2_Protein.Rda"))
+load(file.path(diagnostic_model_FOLDER, "AIFI-Metadata.Rda"))
 data <- data[which(row.names(data)%in%CVD_list$gene),]
 palmo_obj <- createPALMOobject(anndata=ann, data=data)
 palmo_obj<- annotateMetadata(data_object=palmo_obj,sample_column= "Sample", donor_column= "PTID",time_column= "Time")
@@ -25,13 +32,11 @@ palmo_obj <- cvCalcBulk(data_object=palmo_obj, meanThreshold=1, cvThreshold=10,f
 
 
 #####sc-RNA#####
-pbmc <- readRDS("data/AIFI-scRNA-PBMC-FinalData.RDS")
-CVD_list <- fread('CVD-stable.list',data.table=F)
+pbmc <- readRDS(file.path(diagnostic_model_FOLDER,"AIFI-scRNA-PBMC-FinalData.RDS"))
 metaData <- pbmc@meta.data
 pbmc@meta.data$Sample <- pbmc@meta.data$orig.ident
 pbmc@meta.data$celltype <- gsub(" ", "_", pbmc@meta.data$celltype)
-load("data/AIFI-Metadata.Rda")
-library("Seurat")
+load(file.path(diagnostic_model_FOLDER,"/AIFI-Metadata.Rda"))
 vgGroup <- "celltype"
 cell_type <- sort(unique(pbmc@meta.data$celltype))
 celltype_oi <- c("CD4-Naive","CD4-TEM","CD4-TCM","CD4-CTL","CD8-Naive","CD8-TEM","CD8-TCM","Treg","MAIT","gdT","NK", "NK-CD56bright","B-naive", "B-memory", "B-intermediate","CD14-Mono","CD16-Mono","cDC2","pDC")
@@ -44,5 +49,4 @@ featureSet <- c("PTID", "Time","celltype")
 palmo_obj <- lmeVariance(data_object=palmo_obj,featureSet=featureSet,meanThreshold=0.1, cl=4,fileName="CVDsc-10_all")
 var_decomp <- palmo_obj@result$variance_decomposition
 palmo_obj <- cvCalcSC(data_object=palmo_obj,meanThreshold=0.1, cvThreshold=10,fileName="CVDsc-10_all")
-palmo_obj <- StableFeatures(data_object=palmo_obj, group_oi=celltype_oi,cvThreshold=10,donorThreshold=4, groupThreshold=40,topFeatures=25,fileName="CVDsc-10_all",gene_list=CVD_list)
-
+palmo_obj <- StableFeatures(data_object=palmo_obj, group_oi=celltype_oi,cvThreshold=10,donorThreshold=4, groupThreshold=40,topFeatures=25,fileName="CVDsc-10_all")
